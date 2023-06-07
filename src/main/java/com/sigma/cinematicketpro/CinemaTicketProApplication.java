7cc7de21ee218ebdf66a890d3603f58379d06ea2
oleksii.horobets <alexeyhorobets@gmail.com>
2023-06-07 15:08:13 +0300

    Initial commit

---
+++ src/main/java/com/sigma/cinematicketpro/CinemaTicketProApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.cinematicketpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sigma/cinematicketpro/CinemaTicketProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.cinematicketpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
