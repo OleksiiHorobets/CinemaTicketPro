d58df13bd8e449a81869dcf80eac7b0420aee155
oleksii.horobets <alexeyhorobets@gmail.com>
2023-06-13 15:39:42 +0300

    added test for sayHi()

--- src/test/java/com/sigma/cinematicketpro/CinemaTicketProApplicationTests.java
+++ src/test/java/com/sigma/cinematicketpro/CinemaTicketProApplicationTests.java
@@ -1,9 +1,11 @@
 package com.sigma.cinematicketpro;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CinemaTicketProApplicationTests {
@@ -12,7 +14,11 @@ class CinemaTicketProApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.assertNotNull(controller);
+		assertNotNull(controller);
 	}
 
+	@Test
+	void sayHi() {
+		assertEquals("Hello, world!", controller.sayHi());
+	}
 }
