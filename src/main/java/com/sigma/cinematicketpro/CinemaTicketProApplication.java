c84070cae25b7b26de84923101b63be688a4c178
oleksii.horobets <alexeyhorobets@gmail.com>
2023-06-13 14:01:14 +0300

    Added sayHi function

--- src/main/java/com/sigma/cinematicketpro/CinemaTicketProApplication.java
+++ src/main/java/com/sigma/cinematicketpro/CinemaTicketProApplication.java
@@ -2,7 +2,12 @@ package com.sigma.cinematicketpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class CinemaTicketProApplication {
 
@@ -10,4 +15,8 @@ public class CinemaTicketProApplication {
 		SpringApplication.run(CinemaTicketProApplication.class, args);
 	}
 
+	@GetMapping
+	public String sayHi() {
+		return "Hello, world!";
+	}
 }
